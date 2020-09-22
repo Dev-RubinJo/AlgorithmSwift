5cb3a1245a36b2119191aa443eb6c5410211c22a
Rubin.Jo <dbqlsdl1234@naver.com>
2020-09-23 02:01:57 +0900

    테스트 파일 생성

---
+++ AlgorithmSwift/AlgorithmSwift/test.swift
@@ -0,0 +1,19 @@
+//
+//  test.swift
+//  AlgorithmSwift
+//
+//  Created by YooBin Jo on 2020/09/14.
+//  Copyright © 2020 YooBin Jo. All rights reserved.
+//
+
+import Foundation
+
+let a = 123
+func test() {
+    let a = readLine()
+    
+    print(a!)
+    print(Int(a!))
+}
+test()
+print(123)
