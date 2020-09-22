97a1c6645e74fb9296853b983ef39572cfe450a1
Rubin.Jo <dbqlsdl1234@naver.com>
2020-09-23 02:01:44 +0900

    기본 프로젝트 생성

---
+++ AlgorithmSwift/AlgorithmSwift/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  AlgorithmSwift
+//
+//  Created by YooBin Jo on 2020/09/14.
+//  Copyright © 2020 YooBin Jo. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+test()
