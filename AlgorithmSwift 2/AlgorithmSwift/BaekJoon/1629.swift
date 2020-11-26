2971a11c999ac52af9764e197359df480a1caad2
Rubin.Jo <dbqlsdl1234@naver.com>
2020-11-26 13:38:44 +0900

    백준 1629 해결중(런타임 에러 잡는중)

---
+++ AlgorithmSwift 2/AlgorithmSwift/BaekJoon/1629.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+func power(_ a: Int, _ b: Int, _ c: Int) -> Int {
+    if b == 1 {
+        return a % c
+    } else {
+        let temp = power(a, b / 2, c)
+            if b % 2 == 0 {
+                return temp * temp % c
+            } else {
+                return temp * temp * a % c
+            }
+    }
+}
+
+let abc = readLine()!.components(separatedBy: " ").map { value in return Int(value)! }
+
+let a = abc[0]
+let b = abc[1]
+let c = abc[2]
+
+print(power(a, b, c))
