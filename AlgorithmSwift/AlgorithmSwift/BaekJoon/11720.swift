434eb9fa1a0ab59ad6b1415f8c59f2436c08806e
Rubin.Jo <dbqlsdl1234@naver.com>
2020-12-09 14:32:45 +0900

    백준 11720 수정

--- AlgorithmSwift/AlgorithmSwift/BaekJoon/11720.swift
+++ AlgorithmSwift/AlgorithmSwift/BaekJoon/11720.swift
@@ -11,7 +11,8 @@ let numbers = readLine()!
 var ans: Int = 0
 
 for i in 0 ..< n {
-    ans += Int(numbers[numbers.indexNum(i)])!
+    let number = numbers[numbers.indexNum(i)]
+    ans += Int(String(number))!
 }
 
 print(ans)
